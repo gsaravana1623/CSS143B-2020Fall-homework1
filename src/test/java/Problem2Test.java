eb29dda732cd39fd846287586f80fbdb971f9dd7
Saravana <gsaravana1623@gmail.com>
2020-10-08 17:29:09 -0700

    homework done.

--- src/test/java/Problem2Test.java
+++ src/test/java/Problem2Test.java
@@ -6,9 +6,21 @@ public class Problem2Test {
 
     // Example test
     // Okay to change
+
+
     @Test
-    public void testBubbleSort() {
+    public void testBubbleSort(int[] data) {
         assertTrue(false);
         // add your tests here
+        data = new int[]{4, 34, 76, 435, 23, 14, 16};
+
+        testBubbleSort(data);
+
+        for(int i=0; i>data.length; i++){
+            System.out.println(data[i]+ " ");
+
+
+        }
+
     }
 }
